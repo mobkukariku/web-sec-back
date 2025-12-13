d7619af8366fe111e44bb9a72eee7f3aaf0b7852
Damir Tagankhozhaev <tagankhozhaev@gmail.com>
2025-12-13 18:23:08 +0500

    backend v1.0

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3001;
+const port = 3002;
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -19,14 +19,15 @@ const pool = new Pool({
 });
 
 //endpoints
+
 app.post('/api/register', async (req, res) => {
     const {email, password} = req.body;
     try{
         const hashedPassword = await bcrypt.hash(password, 10);
-        await pool.query('INSERT INTO users (email, password) VALUES ($1, $2)', [email, hashedPassword]);
+        await pool.query('INSERT INTO users (email, password_hash) VALUES ($1, $2)', [email, hashedPassword]);
         res.json({ message: 'Пользователь зарегистрирован' });
     }catch(err){
-        res.status(400).json({ error: err.message });
+        res.status(400).json({ error: err });
     }
 });
 
@@ -38,7 +39,7 @@ app.post('/api/login', async (req, res) => {
 
         if(!user) return res.status(400).json({ error: 'User not found' });
 
-        const validPassword = await bcrypt.compare(password, user.password);
+        const validPassword = await bcrypt.compare(password, user.password_hash);
 
         if(!validPassword) return res.status(400).json({ error: 'Invalid password' });
 
@@ -52,8 +53,7 @@ app.get('/api/recipes', async (req, res) => {
     const {search} = req.query;
     try{
         const result = await pool.query(
-            `SELECT * FROM recipes WHERE title ILIKE $1`,
-            [`%${search}%`]
+            `SELECT * FROM recipes`
         );
 
         res.json(result.rows);
@@ -62,41 +62,75 @@ app.get('/api/recipes', async (req, res) => {
     }
 })
 
+app.get('/api/recipes/:id', async (req, res) => {
+    const {id} = req.params;
+    try{
+        const result = await pool.query(
+            `SELECT r.*,
+                    COALESCE(
+                        json_agg(
+                            json_build_object(
+                                'ingredient_id', ri.ingredient_id,
+                                'amount', ri.amount,
+                                'name', i.name
+                            )
+                        ) FILTER (WHERE ri.ingredient_id IS NOT NULL),
+                        '[]'
+                    ) as ingredients
+             FROM recipes r
+             LEFT JOIN recipe_ingredients ri ON r.id = ri.recipe_id
+             LEFT JOIN ingredients i ON ri.ingredient_id = i.id
+             WHERE r.id = $1
+             GROUP BY r.id`, [id]
+        );
+
+        res.json(result.rows[0]);
+    }catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+})
+
 
 app.post('/api/recipes', async (req, res) => {
-    const { userId, title, description, duration, difficulty, ingredientIds } = req.body;
+    const { title, description, duration, author_id, ingredients } = req.body;
 
     try{
-        await pool.query('BEGIN');
 
-        const recipeRes = await pool.query(
-            `INSERT INTO recipes (title, description, duration, difficulty)
-       VALUES ($1, $2, $3, $4) RETURNING id`,
-            [title, description, duration, difficulty]
-        );
-        const recipeId = recipeRes.rows[0].id;
+        pool.query('BEGIN');
 
-        // Связь с пользователем
-        await pool.query(
-            `INSERT INTO user_recipe (user_id, recipe_id) VALUES ($1, $2)`,
-            [userId, recipeId]
-        );
+        const recipeQuery = `
+          INSERT INTO recipes (title, description, duration, author_id)
+          VALUES ($1, $2, $3, $4)
+          RETURNING id
+        `;
 
-        // Добавление ингредиентов
-        for (let ingId of ingredientIds) {
-            await pool.query(
-                `INSERT INTO recipe_ingredients (recipe_id, ingredient_id) VALUES ($1, $2)`,
-                [recipeId, ingId]
-            );
+        const recipeResult = await pool.query(recipeQuery, [title, description, duration, author_id]);
+        const newRecipeId = recipeResult.rows[0].id;
+
+        const ingredientQuery = `
+          INSERT INTO recipe_ingredients (recipe_id, ingredient_id, amount)
+          VALUES ($1, $2, $3)
+        `;
+
+        for (const item of ingredients) {
+            await pool.query(ingredientQuery, [newRecipeId, item.id, item.amount]);
         }
 
         await pool.query('COMMIT');
-        res.json({ message: 'Рецепт создан', recipeId });
+
+
+        res.status(201).json({
+            message: "Рецепт и ингредиенты успешно созданы",
+            recipeId: newRecipeId
+        });
+
     }catch (err) {
         await pool.query('ROLLBACK');
         res.status(500).json({ error: err.message });
     }
 })
+
+
 
 
 app.get('/api/ingredients', async (req, res) => {
